3d02edb7fbb72fa1245248690362cd7d1fe54e68
catapplesofa <166747414+catapplesofa@users.noreply.github.com>
2024-12-17 00:08:11 -0500

    get rid of b

--- banner_grabber.py
+++ banner_grabber.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+import socket
+
 def banner(ip, port):
     s = socket.socket()
-    s.connect((ip,[int(port)]))
-    print(s.recv(1024))
+    s.connect((ip,int(port)))
+    print(str(s.recv(1024)).strip('b'))
 
 def main():
     ip = input("Please enter the IP: ")
